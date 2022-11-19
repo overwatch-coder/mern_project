4d954550099683ffed83339d25f9d3355581bd61
Nyamadi Atsu <devbrainy98@gmail.com>
2022-11-19 20:41:22 +0100

    changed details

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ app.use(bodyParser.json({limit: '100mb'}));
 app.use(bodyParser.urlencoded({limit: '100mb', extended: true, parameterLimit: 1000000, type: 'application/x-www-form-urlencoded'}));
 app.use(express.json({limit: '100mb'}));
 app.use(cors());
-
+const port = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_DB_URI).then(() => {
-    app.listen(process.env.PORT || 5000, (err) => {
+    app.listen(port, (err) => {
         if(err) throw Error('Error: ', err.message);
-        console.log('Connected to DB and running on port: ' + process.env.PORT);
+        console.log('Connected to DB and running on port: ' + port);
     })
 });
 
